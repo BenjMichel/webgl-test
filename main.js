7662c0084d44b749bf1d19049b8534b7259f1122
Benjamin Michel <benjaminm@bamlab.fr>
2016-09-25 01:45:29 +0200

    Particles using webGL renderer

--- main.js
+++ main.js
@@ -1,70 +1,109 @@
-// var scene = new THREE.Scene();
-// var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+var camera;
+var scene;
+var renderer;
+var cubeMesh;
 
-// var renderer = new THREE.WebGLRenderer();
-// renderer.setSize( window.innerWidth, window.innerHeight );
-// document.body.appendChild( renderer.domElement );
+var clock;
+var deltaTime;
 
-var container, stats;
-var scene, camera, group, particle;
-var mouseX = 0, mouseY = 0;
-var windowHalfX = window.innerWidth / 2;
-var windowHalfY = window.innerHeight / 2;
+var particleSystem;
+
 init();
-// animate();
+animate();
 
 function init() {
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
-  camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 3000 );
-  camera.position.z = 1000;
+
+  clock = new THREE.Clock(true);
+
   scene = new THREE.Scene();
-  var PI2 = Math.PI * 2;
-  var program = function ( context ) {
-    context.beginPath();
-    context.arc( 0, 0, 0.5, 0, PI2, true );
-    context.fill();
-  };
-  group = new THREE.Group();
-  scene.add( group );
-  for ( var i = 0; i < 1000; i++ ) {
-    var material = new THREE.SpriteCanvasMaterial( {
-      color: Math.random() * 0.001 * 0xf4eba6 + 0xf4eba6,
-      program: program
-    } );
-    particle = new THREE.Sprite( material );
-    particle.position.x = Math.random() * 2000 - 1000;
-    particle.position.y = Math.random() * 2000 - 1000;
-    particle.position.z = Math.random() * 2000 - 1000;
-    particle.scale.x = particle.scale.y = Math.random() * 20 + 10;
-    group.add( particle );
-  }
-  renderer = new THREE.CanvasRenderer();
-  renderer.setPixelRatio( window.devicePixelRatio );
+  camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 1, 1000);
+  camera.position.z = 50;
+
+  var light = new THREE.DirectionalLight( 0xffffff );
+  light.position.set( 1, -1, 1 ).normalize();
+  scene.add(light);
+
+  var geometry = new THREE.CubeGeometry( 10, 10, 10);
+  var material = new THREE.MeshPhongMaterial( { color: 0x0033ff, specular: 0x555555, shininess: 30 } );
+
+  cubeMesh = new THREE.Mesh(geometry, material );
+  cubeMesh.position.z = -30;
+  scene.add( cubeMesh );
+
+  renderer = new THREE.WebGLRenderer();
   renderer.setSize( window.innerWidth, window.innerHeight );
-  container.appendChild( renderer.domElement );
-  stats = new Stats();
-  container.appendChild( stats.dom );
-  // document.addEventListener( 'mousemove', onDocumentMouseMove, false );
-  // document.addEventListener( 'touchstart', onDocumentTouchStart, false );
-  // document.addEventListener( 'touchmove', onDocumentTouchMove, false );
-  // window.addEventListener( 'resize', onWindowResize, false );
+  document.body.appendChild( renderer.domElement );
+
+  window.addEventListener( 'resize', onWindowResize, false );
+
+  render();
 }
 
-// function animate() {
-//   requestAnimationFrame( animate );
-//   render();
-//   stats.update();
-// }
+function animate() {
+  deltaTime = clock.getDelta();
 
-var render = function () {
-  requestAnimationFrame( render );
+  cubeMesh.rotation.x += 1 * deltaTime;
+  cubeMesh.rotation.y += 2 * deltaTime;
 
-  group.rotation.x += 0.01;
-  group.rotation.y += 0.02;
+  render();
+  requestAnimationFrame(animate);
+}
 
+
+function render() {
+  if (particleSystem) {
+    particleSystem.rotation.x += 0.01;
+    particleSystem.rotation.y += 0.02;
+  }
   renderer.render(scene, camera);
-  stats.update();
-};
+}
 
-render();
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  render();
+}
+
+function createParticleSystem() {
+
+  // The number of particles in a particle system is not easily changed.
+  var particleCount = 2000;
+
+  // Particles are just individual vertices in a geometry
+  // Create the geometry that will hold all of the vertices
+  var particles = new THREE.Geometry();
+
+  // Create the vertices and add them to the particles geometry
+  for (var p = 0; p < particleCount; p++) {
+
+    // This will create all the vertices in a range of -200 to 200 in all directions
+    var x = Math.random() * 400 - 200;
+    var y = Math.random() * 400 - 200;
+    var z = Math.random() * 400 - 200;
+
+    // Create the vertex
+    var particle = new THREE.Vector3(x, y, z);
+
+    // Add the vertex to the geometry
+    particles.vertices.push(particle);
+  }
+
+  // Create the material that will be used to render each vertex of the geometry
+  var particleMaterial = new THREE.PointsMaterial(
+      {color: 0xffffff,
+       size: 4,
+       map: THREE.ImageUtils.loadTexture("images/snowflake.png"),
+       blending: THREE.AdditiveBlending,
+       transparent: true,
+      });
+
+  // Create the particle system
+  particleSystem = new THREE.Points(particles, particleMaterial);
+
+  return particleSystem;
+}
+
+particleSystem = createParticleSystem();
+scene.add(particleSystem);
